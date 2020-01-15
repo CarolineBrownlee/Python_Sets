291f03ae0aeec713b2f042f30ba6a551c44a3dc9
Caroline Brownlee <brownleecaroline@gmail.com>
2020-01-15 05:17:43 -0600

    finished exercises for chapter

--- cars.py
+++ cars.py
@@ -40,3 +40,24 @@ print(showroom)
 showroom.discard("Range Rover")
 print(showroom)
 
+# Acquiring more cars:
+# 1. Now create another set of cars in a variable junkyard. Someone who owns a junkyard full of old cars has approached you about buying the entire inventory. In the new set, add some different cars, but also add a few that are the same as in the showroom set.
+junkyard = set()
+junkyard.add("Leaf")
+junkyard.add("Durango")
+junkyard.add("Jetta")
+junkyard.add("Pilot")
+print(junkyard)
+
+# 2. Use the intersection method to see which cars exist in both the showroom and that junkyard.
+intersection = showroom.intersection(junkyard)
+print(intersection)
+
+# 3. Now you're ready to buy the cars in the junkyard. Use the union method to combine the junkyard into your showroom.
+union = showroom.union(junkyard)
+print(union)
+
+# 4. Use the discard() method to remove any cars that you acquired from the junkyard that you do not want in your showroom.
+union.discard("Leaf")
+union.discard("Durango")
+print(union)
